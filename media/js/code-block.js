4af957b1441f7f86c78b26d8d045ebedd26b506b
Plamen Svetoslavov <svetoslavov.plamen@gmail.com>
2026-01-02 21:20:52 +0200

    ADD: Init commit

---
+++ media/js/code-block.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const codeBlock = document.querySelector(".code-block");
+    if (!codeBlock) return;
+
+    let isActive = false;
+
+    const observer = new IntersectionObserver(
+        ([entry]) => {
+
+            // ENTER: 90% visible
+            if (entry.intersectionRatio >= 0.96 && !isActive) {
+                codeBlock.classList.add("is-active");
+                document.body.classList.add("is-dark-mode");
+                isActive = true;
+            }
+
+            // EXIT: completely out
+            if (entry.intersectionRatio === 0 && isActive) {
+                codeBlock.classList.remove("is-active");
+                document.body.classList.remove("is-dark-mode");
+                isActive = false;
+            }
+
+        },
+        {
+            threshold: [0, 0.96]
+        }
+    );
+
+    observer.observe(codeBlock);
+});
