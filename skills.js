d8c19e5df79782b57c406f4d4985d0753f7af833
Plamen Svetoslavov <svetoslavov.plamen@gmail.com>
2026-01-02 23:26:15 +0200

    UPDATE

--- skills.js
+++ skills.js
@@ -1,18 +1,18 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const skillsGrid = document.querySelector(".skills-grid");
-    if (!skillsGrid) return;
-
-    const observer = new IntersectionObserver(
-        ([entry]) => {
-            if (entry.intersectionRatio >= 0.4) {
-                skillsGrid.classList.add("is-active");
-                observer.disconnect(); // 🔑 само веднъж
-            }
-        },
-        {
-            threshold: [0, 0.4, 1]
-        }
-    );
-
-    observer.observe(skillsGrid);
-});
+// document.addEventListener("DOMContentLoaded", () => {
+//     const skillsGrid = document.querySelector(".skills-grid");
+//     if (!skillsGrid) return;
+//
+//     const observer = new IntersectionObserver(
+//         ([entry]) => {
+//             if (entry.intersectionRatio >= 0.4) {
+//                 skillsGrid.classList.add("is-active");
+//                 observer.disconnect(); // 🔑 само веднъж
+//             }
+//         },
+//         {
+//             threshold: [0, 0.4, 1]
+//         }
+//     );
+//
+//     observer.observe(skillsGrid);
+// });
