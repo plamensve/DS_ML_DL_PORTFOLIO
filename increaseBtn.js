5c5de8966ca8754801c895a960070de3d145ad2c
Plamen Svetoslavov <svetoslavov.plamen@gmail.com>
2026-01-06 14:45:15 +0200

    UPDATE: 06.01.2026

--- increaseBtn.js
+++ increaseBtn.js
@@ -1,9 +1,35 @@
-const increaseBtn = document.getElementById('increaseBtn')
-const countLabel = document.getElementById('countLabel')
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  onValue,
+  runTransaction
+} from "https://www.gstatic.com/firebasejs/12.7.0/firebase-database.js";
 
-let count = 0;
+const firebaseConfig = {
+  apiKey: "AIzaSyCK4sfpug2imOKkBLylWmIlyIAmtIxSldE",
+  authDomain: "portfolio-f4408.firebaseapp.com",
+  projectId: "portfolio-f4408",
+  databaseURL: "https://portfolio-f4408-default-rtdb.europe-west1.firebasedatabase.app",
+  storageBucket: "portfolio-f4408.firebasestorage.app",
+  messagingSenderId: "961129077522",
+  appId: "1:961129077522:web:3c3267163509e20af631cb"
+};
 
-increaseBtn.onclick = function (){
-    count++;
-    countLabel.textContent = count;
-}
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+const countLabel = document.getElementById("countLabel");
+const increaseBtn = document.getElementById("increaseBtn");
+
+const counterRef = ref(db, "cv_downloads");
+
+onValue(counterRef, (snapshot) => {
+  countLabel.textContent = snapshot.val() ?? 0;
+});
+
+increaseBtn.addEventListener("click", () => {
+  runTransaction(counterRef, (current) => {
+    return (current ?? 0) + 1;
+  });
+});
